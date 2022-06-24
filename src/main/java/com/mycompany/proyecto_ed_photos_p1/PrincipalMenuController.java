4d749baf03cfd266af434e7d85ecd8438fd61af4
rexman10 <jaguadal@espol.edu.ec>
2022-06-24 15:44:48 -0500

    interfaz refinada

--- src/main/java/com/mycompany/proyecto_ed_photos_p1/PrincipalMenuController.java
+++ src/main/java/com/mycompany/proyecto_ed_photos_p1/PrincipalMenuController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
@@ -62,6 +63,24 @@ public class PrincipalMenuController implements Initializable {
     private Pane pFullview;
     @FXML
     private FlowPane fpGridView;
+    @FXML
+    private Label lbEtiquetas;
+    @FXML
+    private ToggleGroup reaction;
+    @FXML
+    private ComboBox<?> cbAlbum;
+    @FXML
+    private Label lbLugar;
+    @FXML
+    private RadioButton rbLike;
+    @FXML
+    private RadioButton ebLove;
+    @FXML
+    private RadioButton rbSad;
+    @FXML
+    private Label lbPersonas;
+    @FXML
+    private Label lbCamara;
 
     /**
      * Initializes the controller class.
